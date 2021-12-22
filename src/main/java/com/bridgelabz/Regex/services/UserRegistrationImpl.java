dcca9343e4e61ae85c0fa08e843c1fdeb36ee7ad
rvpatil1432 <rvpatil1432@gmail.com>
2021-12-22 07:19:06 +0530

    Added : Use case to validate user registration detail using lambda expression

--- src/main/java/com/bridgelabz/Regex/services/UserRegistrationImpl.java
+++ src/main/java/com/bridgelabz/Regex/services/UserRegistrationImpl.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.Regex.services;
 
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 import com.bridgelabz.Regex.common.EmailInvalidException;
@@ -12,10 +13,13 @@ import com.bridgelabz.Regex.interfaces.IUser;
 
 public class UserRegistrationImpl implements IUser {
 	boolean patternMatch;
+	Pattern pattern;
 	@Override
 	public boolean firstNameValidation(User user) {
 		try {
-			patternMatch = Pattern.matches("^[A-Z][a-z]{2,}", user.getFirstName());
+			pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+			Predicate<String> predicate = pattern.asPredicate();
+			patternMatch = predicate.test(user.getFirstName());
 			if(patternMatch == false) {
 				throw new FirstNameInvalidException("Invalid First Name.."); //throwing custom exception
 			}
@@ -28,7 +32,9 @@ public class UserRegistrationImpl implements IUser {
 	@Override
 	public boolean lastNameValidation(User user) {
 		try {
-			patternMatch = Pattern.matches("^[A-Z][a-z]{2,}", user.getLastName());
+			pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+			Predicate<String> predicate = pattern.asPredicate();
+			patternMatch = predicate.test(user.getLastName());
 			if(patternMatch == false) {
 				throw new LastNameInvalidException("Invalid Last Name.."); //throwing custom exception
 			}
@@ -42,7 +48,9 @@ public class UserRegistrationImpl implements IUser {
 	//public boolean eMailValidation(User user) {
 	public boolean eMailValidation(String eMails) {
 		try {
-			patternMatch = Pattern.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)+(\\.{0,0})$", eMails);
+			pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)+(\\.{0,0})$");
+			Predicate<String> predicate = pattern.asPredicate();
+			patternMatch = predicate.test(eMails);
 			if(patternMatch == false) {
 				throw new EmailInvalidException("Invalid Email ID..");//throwing custom exception
 			}
@@ -54,7 +62,9 @@ public class UserRegistrationImpl implements IUser {
 	@Override
 	public boolean phoneNumValidation(User user) {
 		try {
-			patternMatch = Pattern.matches("^[789]\\d{9}$", user.getPhoneNumber());
+			pattern = Pattern.compile("^[789]\\d{9}$");
+			Predicate<String> predicate = pattern.asPredicate();
+			patternMatch = predicate.test(user.getPhoneNumber());
 			if(patternMatch == false) {
 				throw new MobileNumberInvalidEXception("Invalid Mobile Number..");//throwing custom exception
 			}
@@ -67,7 +77,9 @@ public class UserRegistrationImpl implements IUser {
 	@Override
 	public boolean passwordValidation(User user) {
 		try {
-		patternMatch = Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])([a-zA-Z0-9@$!%*?&]{8,})$", user.getPassword());
+		pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])([a-zA-Z0-9@$!%*?&]{8,})$");
+		Predicate<String> predicate = pattern.asPredicate();
+		patternMatch = predicate.test(user.getPassword());
 		if(patternMatch == false) {
 			throw new PasswordInvalidException("Invalid Password..");//throwing custom exception
 		}
